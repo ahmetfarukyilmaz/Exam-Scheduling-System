bac27ec0ae45fba2864597758ef38f09155f6f0b
xbrkyx <brkydnz89@gmail.com>
2021-05-30 10:09:10 +0300

    Fix templates location, add templates

--- exam_scheduling_system/urls.py
+++ exam_scheduling_system/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from exam.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login),
+    path('register/', register),
 ]
